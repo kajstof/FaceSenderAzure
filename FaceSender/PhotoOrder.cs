20f06060ed371947c9d8b6b10bb3b7d5e2b800fc
Krzysztof Krysiak <krysiak86@gmail.com>
2018-01-27 11:36:56 +0100

    Add HttpOrderFormSave.cs

---
+++ FaceSender/PhotoOrder.cs
@@ -0,0 +1,10 @@
+﻿namespace FaceSender
+{
+    internal class PhotoOrder
+    {
+        public string CustomerEmail { get; set; }
+        public string FileName { get; set; }
+        public int RequiredHeight { get; set; }
+        public int RequiredWidth { get; set; }
+    }
+}
